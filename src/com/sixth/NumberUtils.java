6de4cb0c625e498dba2cc5a585b648a1b8fc7fc0
ConnorGoodwill <goodwill.connor@gmail.com>
2023-07-23 14:44:23 +0100

    Initial Push

---
+++ src/com/sixth/NumberUtils.java
@@ -0,0 +1,27 @@
+package com.sixth;
+
+
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+public class NumberUtils {
+    public static boolean isNarcissistic(int number) {
+        String numberString = String.valueOf(number);
+        if(numberString.isEmpty()){
+            return false;
+        }
+        char[] digitChars = numberString.toCharArray();
+        int sum = 0;
+        for(char digit : digitChars){
+            int x = Integer.parseInt(String.valueOf(digit));
+            Double pow = Math.pow(x, numberString.length());
+            x = pow.intValue();
+            sum += x;
+        }
+        if(sum == number){
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/test/com/sixth/NumberUtilsTest.java
@@ -0,0 +1,24 @@
+package test.com.sixth;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.sixth.NumberUtils;
+import org.junit.jupiter.api.Test;
+
+class NumberUtilsTest {
+
+    @Test
+    void exampleTests() {
+
+        assertTrue(NumberUtils.isNarcissistic(0), "0 is narcissistic");
+        assertTrue(NumberUtils.isNarcissistic(1), "1 is  narcissistic");
+        assertTrue(NumberUtils.isNarcissistic(2), "2 is  narcissistic");
+        assertTrue(NumberUtils.isNarcissistic(153), "153 is narcissistic");
+        assertTrue(NumberUtils.isNarcissistic(407), "407 is narcissistic");
+        assertTrue(NumberUtils.isNarcissistic(1634), "1634 is narcissistic");
+        assertFalse(NumberUtils.isNarcissistic(112), "112 is not narcissistic");
+        assertFalse(NumberUtils.isNarcissistic(12), "12 is not narcissistic");
+    }
+
+}
