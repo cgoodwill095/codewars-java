69db67b71586c8342ff3462a4baddeefcafbdfff
ConnorGoodwill <goodwill.connor@gmail.com>
2023-07-23 15:08:31 +0100

    Valid braces method & tests

---
+++ src/com/sixth/BraceChecker.java
@@ -0,0 +1,62 @@
+package com.sixth;
+
+import java.util.LinkedList;
+
+public class BraceChecker {
+    public boolean isValid(String braces) {
+
+        if(braces.length() %2 !=0 || braces.length() <2){
+            return false;
+        }
+
+        LinkedList<Character> parentheses = new LinkedList<>();
+        char[] chars = braces.toCharArray();
+
+        for (char ch: chars) {
+            if (ch == '{' || ch == '(' || ch == '[') {
+                parentheses.add(ch);
+            }
+            else if (ch == '}' || ch == ')' || ch == ']'){
+                if(parentheses.isEmpty()){
+                    return false;
+                }
+                switch(ch){
+                    case '}':
+                        if(parentheses.getLast()!='{'){
+                            return false;
+                        }
+                        else{
+                            parentheses.removeLast();
+                        }
+
+                        break;
+
+                    case ')':
+                        if(parentheses.getLast()!='('){
+                            return false;
+                        }
+                        else{
+                            parentheses.removeLast();
+                        }
+                        break;
+
+                    case ']':
+                        if(parentheses.getLast()!='['){
+                            return false;
+                        }
+                        else{
+                            parentheses.removeLast();
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+        if(parentheses.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
